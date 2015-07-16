963ec792d48c2a14b3fa93c0443442a39d67c779
Matteo Grolla <m.grolla@sourcesense.com>
2015-07-17 01:59:29 +0200

    metrics calculation ok, synchronization problem

--- module.js
+++ module.js
@@ -16,7 +16,7 @@ app.config(function($routeProvider, $locationProvider){
   })
   .when("/queries/:id", {
     controller: "QueryController",
-    templateUrl: "views/queries/show.html"
+    templateUrl: "views/queries/edit.html"
   });
 
   $locationProvider.html5Mode(true);
@@ -26,10 +26,13 @@ app.config(function($routeProvider, $locationProvider){
 app.factory('Query', function(filterFilter){
 
   var queries = [
-    {id:1, userQuery: "regolamento condominiale", targetDocs: ["100", "101", "103"], score:10},
-    {id: 2, userQuery: "immigrazione", targetDocs: ["200", "201", "203"], score:15},
-    {id: 3, userQuery: "quote rosa", targetDocs: ["300", "301", "303"], score:20}
+    //, targetRanks:[1, 3, NaN]
+    {id:0, userQuery: "*:*", targetDocs: ["adata", "asus", "belkin"], score:10},  
+    {id: 1, userQuery: "*:*", targetDocs: ["apple", "belkin", "dell"], targetRanks:[2, 5, NaN], score:15},
+    {id: 2, userQuery: "*:*", targetDocs: ["asus", "dell", "samsung"], targetRanks: [3, 8, NaN], score:20}
   ];
+
+  var seq = 3;
 
   return {
     query: function(params){
@@ -37,19 +40,169 @@ app.factory('Query', function(filterFilter){
     },
     get: function(params){
       return this.query(params)[0];
+    },
+    put: function(id, query){
+      queries[id] = query;
+    },
+    delete: function(id){
+      queries.splice(id,1);
+    },
+    add: function(){
+      var query = {id: seq++}
+      queries.push(query);
+      return query;
     }
   };
 });
 
-app.controller('MessageController', function ($scope) {
-  $scope.message = "This is a model.";
+app.factory('metricsService', function(solrService){
+  return {
+    defaultRank: 100,
+    allTargetRanks: function(queries, k){
+      for(var i=0; i<queries.length; i++){
+        this.targetRanks(queries[i], k);
+      }
+    },
+    targetRanks: function(query, k){
+      solrService.search(query.userQuery, k).then(
+        function(response){
+          var ids2Rank = {};
+          var docs = response.response.docs;
+          for (var i=0; i<docs.length; i++){
+            var id = docs[i].id;
+            ids2Rank[id] = i+1;
+          }
+          var targetDocs = query.targetDocs;
+          var targetRanks = [];
+          for (var i=0; i<targetDocs.length; i++){
+            var id = targetDocs[i];
+            var rank = NaN;
+            if (ids2Rank[id]){
+              rank = ids2Rank[id];
+            }
+            targetRanks.push(rank);
+          }
+          query.targetRanks = targetRanks;
+        });
+    },
+    precisionAt: function(query, k){
+      var targetRanks = query.targetRanks;
+      var precision=0.0;
+      for (var i=0; i<targetRanks.length; i++){
+        var rank = targetRanks[i];
+        if (rank<=k){
+          precision++;
+        }
+      }
+      precision = precision/k;
+      query["precisionAt"] = precision;
+    },
+    meanPrecisionAt: function(queries, k){
+      var meanPrecisionAt = 0.0;
+      for (var i=0; i<queries.length; i++){
+        var query = queries[i];
+        this.precisionAt(query, k);
+        meanPrecisionAt += query["precisionAt"];
+      }
+      meanPrecisionAt /= queries.length;
+      return meanPrecisionAt;
+    },
+    reciprocalRankAt: function(query, k){
+      var targetRanks = query.targetRanks;    
+      var lowestRank = this.defaultRank;
+      for (var i=0; i<targetRanks.length; i++){
+        var rank = targetRanks[i];
+        if (rank < lowestRank){
+          lowestRank = rank;
+        }
+      }
+      query["reciprocalRankAt"] = 1/lowestRank;
+    },
+    meanReciprocalRankAt: function(queries, k){
+      var meanReciprocalRankAt = 0.0;
+      for (var i=0; i<queries.length; i++){
+        var query = queries[i];
+        this.reciprocalRankAt(query, k);
+        var reciprocalRankAt = query["reciprocalRankAt"];
+        meanReciprocalRankAt += reciprocalRankAt;
+      }
+      return meanReciprocalRankAt / queries.length;
+    }
+  }
 });
 
-app.controller('QueriesController', function ($scope, Query){
-  $scope.queries = Query.query();
+app.value('collection-url', 'http://localhost:8080');
+
+app.factory('solrService', function($q, $http){
+  return {
+    search: function(query, rows){
+      return $http.get('solr/query?q='+query+'&rows='+rows)
+        .then(
+          function(response){
+            return response.data;
+          }, function(errResponse){
+            return $q.reject(errResponse.status + " " +errResponse.data.error.msg);
+          });
+    }
+  }
 });
 
-app.controller('QueryController', function ($scope, Query, $routeParams){
+app.controller('QueriesController', function ($scope, Query, metricsService, solrService, $location){
+  var queries = Query.query();
+  $scope.queries = queries;
+
+  metricsService.allTargetRanks(queries,5);
+  /*
+    Problema: le seguenti 2 chiamate dovrebbero essere eseguite solo dopo che 
+    la precedente chiamata asincrona è stata terminata
+  */
+  var meanPrecisionAt = metricsService.meanPrecisionAt(queries,5);
+  var meanReciprocalRankAt = metricsService.meanReciprocalRankAt(queries, 5);
+  //metricsService.precisionAt(queries[1],5);
+  solrService.search('*:*',3).then(
+    function(docs){
+      //$scope.docs = docs;
+    }, function(err){
+      $scope.docs = err;
+    });
+
+  $scope.meanPrecisionAt = meanPrecisionAt;  
+  $scope.meanReciprocalRankAt = meanReciprocalRankAt;
+
+  $scope.deleteRow = function(id){
+    Query.delete(id);
+    $scope.queries = Query.query();
+  };
+
+  $scope.addRow = function(){
+    var query = Query.add();
+    $location.path("/queries/"+query.id);
+  }
+});
+
+app.controller('QueryController', function ($scope, Query, $routeParams, $location){
   $scope.query = Query.get({id: $routeParams.id});
   var i=0;
+  $scope.save = function(query){    
+    var strToStrArray = function(str){
+      if (Array.isArray(str)){
+        return str;
+      }
+      return str.split(',');
+    };
+    var strToIntArray = function(str){
+      if (Array.isArray(str)){
+        return str;
+      }
+      return str.split(',').map(function(item) {
+          return parseInt(item, 10);
+        });
+    };
+
+    var id = query.id;
+    query.targetDocs = strToStrArray(query.targetDocs);
+    //query.targetRanks = strToIntArray(query.targetRanks);    
+    Query.put(id,query);
+    $location.path("/queries");
+  }
 });
